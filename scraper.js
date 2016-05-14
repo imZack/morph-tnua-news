455b17adbe2f64afa5b2a5b1d0fbf2d1a57b3a9b
YuLun Shih <rezacks@gmail.com>
2016-05-14 17:03:28 +0800

    update

--- scraper.js
+++ scraper.js
@@ -8,14 +8,14 @@ function initDatabase(callback) {
 	// Set up sqlite database.
 	var db = new sqlite3.Database("data.sqlite");
 	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
+		db.run("CREATE TABLE IF NOT EXISTS data (date TEXT, title TEXT, hits TEXT)");
 		callback(db);
 	});
 }
 
 function updateRow(db, value) {
 	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
+	var statement = db.prepare("INSERT INTO data VALUES ($date, $title, $hits)");
 	statement.run(value);
 	statement.finalize();
 }
@@ -44,9 +44,19 @@ function run(db) {
 	fetchPage("http://1www.tnua.edu.tw/news/news.php?class=102", function (body) {
 		// Use cheerio to find things in the page with css selectors.
 		var $ = cheerio.load(body);
-		var elements = $('#RSS_Table_page_news_1 > tbody > tr').each(function (i, item) {
-			var value = $(item).text();
-			updateRow(db, value);
+		var elements = $('#RSS_Table_page_news_1 > tbody > tr').each(function (i, row) {
+			var obj = {};
+			$(row).children().each(function (i, value) {
+				if (i === 1) {
+					obj.date = $(value).text;
+				} else if (i === 2) {
+					obj.title = $(value).text;
+				} else if (i === 3){
+					obj.hits = $(value).text;
+				}
+			});
+
+			updateRow(db, obj);
 		});
 
 		readRows(db);
